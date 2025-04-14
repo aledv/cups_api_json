36f0fcab4665659e4ce1e38bdac58fcc85a4deea
Alessandro Di Vanni <alessandro.divanni@gmail.com>
2025-04-14 19:12:03 +0000

    added info status

--- app.py
+++ app.py
@@ -24,13 +24,32 @@ def get_pending_jobs_count(printer):
     pending_jobs = run_command(['lpstat', '-W', 'not-completed', printer])
     return len(pending_jobs)
 
+def get_printer_status(printer):
+    output = run_command(['lpstat', '-p', printer])
+    if output:
+        # Di solito la prima riga ha qualcosa come:
+        # "printer HP_LaserJet is idle.  enabled since ..."
+        line = output[0].lower()
+        if "disabled" in line:
+            return "disabled"
+        elif "idle" in line:
+            return "idle"
+        elif "printing" in line:
+            return "printing"
+        elif "paused" in line:
+            return "paused"
+        else:
+            return "unknown"
+    return "not found"
+
 @app.route('/cups/status')
 def cups_status():
     default_printer = get_default_printer()
     return jsonify({
         'default_printer': default_printer,
         'completed_jobs_count': get_completed_jobs_count(default_printer) if default_printer else 0,
-        'pending_jobs_count': get_pending_jobs_count(default_printer) if default_printer else 0
+        'pending_jobs_count': get_pending_jobs_count(default_printer) if default_printer else 0,
+        'printer_status': get_printer_status(default_printer) if default_printer else "not found"
     })
 
 if __name__ == '__main__':
